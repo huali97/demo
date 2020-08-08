4019cbb7c2086d2fe0378695bd009ca38929643f
QHL <huali.qiu@outlook.com>
2020-08-08 13:54:25 +0800

    one

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/home'
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/news',
+    name: 'News',
+    component: () => import( '../views/news')
+ },
+ {
+   path: '/find',
+   name: 'Find',
+   component: () => import( '../views/find')
+},
+{
+  path: '/serial',
+  name: 'Serial',
+  component: () => import( '../views/serial')
+},
+{
+  path: '/my',
+  name: 'My',
+  component: () => import( '../views/my')
+},
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
